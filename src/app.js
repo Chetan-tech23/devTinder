e68d47d3827c58ecc0a69f49e18009808f586768
Chetan Malpure <ckmalpure23@gmail.com>
2025-10-01 19:15:02 +0530

    API sanitizing

--- src/app.js
+++ src/app.js
@@ -7,15 +7,18 @@ const PORT = 7777;
 
 app.use(express.json());
 
+/**
+ * Creating API - Signup
+ */
 app.post("/signup", async (req, res) => {
   // Creating a instance of the User model
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.send("User is saved successfully!!");
+    res.send("User is registered successfully!!");
   } catch (err) {
-    res.status(400).send("Something error in user saving!!! " + err.message);
+    res.status(400).send("Something went wrong in user register!!! " + err.message);
   }
 });
 
@@ -70,16 +73,32 @@ app.delete("/user", async (req, res) => {
 /**
  * Creating API - Update data of User
  */
- app.patch("/user", async (req, res) => {
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  const data = req.body;
   try {
-    const user = await User.findByIdAndUpdate(req.body.userId, req.body.update);
+    const ALLOWED_FIELDS = ["firstName", "lastName", "age", "gender", "photoUrl", "about", "skills"];
+    const isAllowedUser = Object.keys(data).every(k => ALLOWED_FIELDS.includes(k));
+    if(!isAllowedUser){
+      throw new Error("Update Failed : Random or restricted Fields are not allowd");
+    }
+
+    if(data.skills.length > 10){
+      throw new Error("Update Failed : More than 10 skills not allowed!");
+    }
+
+    const user = await User.findByIdAndUpdate(userId, data, {
+      runValidators: true,
+    });
     if (user.length === 0) {
       res.send("User not found!");
     } else {
       res.send("User updated successfully!");
     }
   } catch (err) {
-    res.status(400).send("Something went wrong!!!");
+    res
+      .status(400)
+      .send("Something went wrong on user updation : " + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,71 @@
 const { Schema, model } = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Enter correct email address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter strong password: " + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      trim: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is specify incorrect..");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      trim: true,
+      default: "https://i.sstatic.net/l60Hf.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Enter valid photo URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      trim: true,
+    },
+    skills: {
+      type: [String],
+      trim: true,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model("User", userSchema);
