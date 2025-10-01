c4732ef80efbd844ada65ba0450965a28f8728fd
Chetan Malpure <ckmalpure23@gmail.com>
2025-10-01 19:18:45 +0530

    solved warnings

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   mongoose.set("useCreateIndex", true);
+  mongoose.set('useFindAndModify', false);
 
   await mongoose.connect(
     "mongodb+srv://ChetanDev:ChetanDev23@chetandev.abdyvlq.mongodb.net/devTinder",
