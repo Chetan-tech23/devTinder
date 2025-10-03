b405df81ed6c73c22eb433d1cbe30b634aa65bf1
Chetan Malpure <ckmalpure23@gmail.com>
2025-10-03 16:07:49 +0530

    Fixed warnings

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  mongoose.set("useCreateIndex", true);
-  mongoose.set('useFindAndModify', false);
-
   await mongoose.connect(
-    "mongodb+srv://ChetanDev:ChetanDev23@chetandev.abdyvlq.mongodb.net/devTinder",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    "mongodb+srv://ChetanDev:ChetanDev23@chetandev.abdyvlq.mongodb.net/devTinder"
   );
 };
 
